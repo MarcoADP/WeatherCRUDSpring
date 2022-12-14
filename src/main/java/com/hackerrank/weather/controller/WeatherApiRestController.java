0d56ec006de49a50964197d8f2f98c432943c084
Admin <admin.push@hackerrank.workspace>
2022-12-14 12:44:06 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- src/main/java/com/hackerrank/weather/controller/WeatherApiRestController.java
+++ src/main/java/com/hackerrank/weather/controller/WeatherApiRestController.java
@@ -1,7 +1,41 @@
 package com.hackerrank.weather.controller;
 
+import javax.validation.Valid;
+
+import com.hackerrank.weather.model.Weather;
+import com.hackerrank.weather.repository.WeatherRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path="/weather")
 public class WeatherApiRestController {
+
+  @Autowired
+  WeatherRepository weatherRepository;
+
+  @PostMapping
+  public ResponseEntity create(@Valid @RequestBody Weather params) {
+
+    Weather weather = new Weather(
+      params.getDateRecorded(), 
+      params.getCity(),
+      params.getState(), 
+      params.getLat(),
+      params.getLon(),
+      params.getTemperature()
+    );
+
+    return new ResponseEntity<Weather>(weatherRepository.save(weather), HttpStatus.CREATED);
+
+  }
+
+
 }
